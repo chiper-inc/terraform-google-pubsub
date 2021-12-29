78345d8a91c4275254ed9f6df26fb3f3cbbea542
Diego Alves <dfqalves@gmail.com>
2021-12-29 08:45:17 -0300

    CORE-232 service account

--- main.tf
+++ main.tf
@@ -1,49 +1,15 @@
 
-# Get the current GCP project
+# CONTEXT DATA
 data "google_project" "project" {
 }
 
-# Default values
+# LOCAL VALUES
 locals {
   message_retention_duration   = "604800s"
   default_message_ordering     = true
   default_ack_deadline_seconds = 30
   pubsub_svc_account_email     = "service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
 }
-
-# Adds the Token Creator role to pubsub service account
-# Then the push subscriber can use authentication
-# resource "google_project_iam_member" "token_creator_binding" {
-
-#   project = data.google_project.project.id
-#   role    = "roles/iam.serviceAccountTokenCreator"
-#   member  = "serviceAccount:${local.pubsub_svc_account_email}"
-#   depends_on = [
-#     google_pubsub_subscription.push_subscriptions,
-#   ]
-# }
-
-# resource "google_pubsub_topic_iam_member" "publisher_role" {
-#   project = data.google_project.project.project_id
-#   role    = "roles/pubsub.publisher"
-#   topic   = google_pubsub_topic.dlq.id
-#   member  = "serviceAccount:${local.pubsub_svc_account_email}"
-#   depends_on = [
-#     google_pubsub_topic.topic,
-#   ]
-# }
-
-# resource "google_pubsub_subscription_iam_member" "subscriber_role" {
-#   for_each = { for i in var.push_subscriptions : i.name => i }
-
-#   project      = data.google_project.project.project_id
-#   subscription = each.value.name
-#   role         = "roles/pubsub.subscriber"
-#   member       = "serviceAccount:${local.pubsub_svc_account_email}"
-#   depends_on = [
-#     google_pubsub_subscription.push_subscriptions,
-#   ]
-# }
 
 ###
 # TOPICS
@@ -62,8 +28,8 @@ resource "google_pubsub_topic" "dlq" {
 }
 
 ## 
-# Subscriptions 
-
+# PUSH SUBSCRIPTIONS 
+##
 resource "google_pubsub_subscription" "push_subscriptions" {
   for_each = { for i in var.push_subscriptions : i.name => i }
 
@@ -87,6 +53,10 @@ resource "google_pubsub_subscription" "push_subscriptions" {
   dead_letter_policy {
     dead_letter_topic     = google_pubsub_topic.dlq.id
     max_delivery_attempts = 5
+  }
+
+  oidc_token {
+    service_account_email = each.value["service_account"]
   }
 
 }
